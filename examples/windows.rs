1c87778c3556d1789c2bfaaec9cd2a21e7845794
sidit77 <sidit77@gmail.com>
2023-10-28 13:35:53 +0200

    more menu work

--- examples/windows.rs
+++ examples/windows.rs
@@ -1,13 +1,51 @@
 use std::mem::size_of;
 use once_cell::sync::Lazy;
 use windows::core::{PCWSTR, w, Result, s};
-use windows::Win32::Foundation::{HINSTANCE, HWND, LPARAM, LRESULT, WPARAM};
+use windows::Win32::Foundation::{HINSTANCE, HWND, LPARAM, LRESULT, POINT, WPARAM};
 use windows::Win32::System::SystemServices::IMAGE_DOS_HEADER;
-use windows::Win32::UI::Shell::{DefSubclassProc, NIF_ICON, NIF_MESSAGE, NIF_TIP, NIM_ADD, NIM_DELETE, NOTIFYICONDATAW, SetWindowSubclass, Shell_NotifyIconW};
-use windows::Win32::UI::WindowsAndMessaging::{CreateWindowExW, DefWindowProcW, DestroyWindow, DispatchMessageW, GetMessageW, HMENU, IDI_QUESTION, LoadIconW, MSG, PostMessageW, RegisterClassW, RegisterWindowMessageA, TranslateMessage, UnregisterClassW, WM_DESTROY, WM_LBUTTONDBLCLK, WM_LBUTTONUP, WM_MBUTTONUP, WM_QUIT, WM_RBUTTONUP, WNDCLASSW, WS_EX_LAYERED, WS_EX_NOACTIVATE, WS_EX_TOOLWINDOW, WS_EX_TRANSPARENT, WS_OVERLAPPED};
+use windows::Win32::UI::Shell::{DefSubclassProc, NIF_ICON, NIF_MESSAGE, NIM_ADD, NIM_DELETE, NOTIFYICONDATAW, SetWindowSubclass, Shell_NotifyIconW};
+use windows::Win32::UI::WindowsAndMessaging::{AppendMenuW, CreateMenu, CreatePopupMenu, CreateWindowExW, DefWindowProcW, DestroyMenu, DestroyWindow, DispatchMessageW, GetCursorPos, GetMessageW, HMENU, HWND_MESSAGE, IDI_QUESTION, LoadIconW, MF_POPUP, MF_SEPARATOR, MF_STRING, MSG, PostMessageW, RegisterClassW, RegisterWindowMessageA, SetForegroundWindow, TPM_BOTTOMALIGN, TPM_LEFTALIGN, TrackPopupMenu, TranslateMessage, UnregisterClassW, WINDOW_EX_STYLE, WINDOW_STYLE, WM_COMMAND, WM_DESTROY, WM_LBUTTONDBLCLK, WM_LBUTTONUP, WM_QUIT, WM_RBUTTONUP, WNDCLASSW};
 
 const TRAY_SUBCLASS_ID: usize = 6001;
 const WM_USER_TRAY_ICON: u32 = 6002;
+
+struct TrayData {
+    menu: Menu
+}
+
+struct Menu {
+    hmenu: HMENU
+}
+
+impl Menu {
+    pub fn new() -> Result<Self> {
+        unsafe {
+            let hmenu = CreatePopupMenu()?;
+
+            let submenu = CreatePopupMenu()?;
+
+            AppendMenuW(submenu, MF_STRING, 39, w!("Profile 1")).unwrap();
+
+            AppendMenuW(hmenu, MF_POPUP, submenu.0 as _, w!("Profiles")).unwrap();
+            AppendMenuW(hmenu, MF_SEPARATOR, 0, None).unwrap();
+            AppendMenuW(hmenu, MF_STRING, 41, w!("Open")).unwrap();
+            AppendMenuW(hmenu, MF_STRING, 42, w!("Quit")).unwrap();
+
+            Ok(Self {
+                hmenu
+            })
+        }
+    }
+}
+
+impl Drop for Menu {
+    fn drop(&mut self) {
+        println!("dropping menu");
+        unsafe {
+            DestroyMenu(self.hmenu).unwrap();
+        }
+    }
+}
 
 fn main() -> Result<()> {
     println!("Hello WOrld");
@@ -30,15 +68,13 @@ fn main() -> Result<()> {
 
     let hwnd = unsafe {
         CreateWindowExW(
-            WS_EX_NOACTIVATE | WS_EX_TRANSPARENT | WS_EX_LAYERED | WS_EX_TOOLWINDOW,
+            WINDOW_EX_STYLE::default(),
             class_name,
             PCWSTR::null(),
-            WS_OVERLAPPED,
-            0,
-            0,
-            0,
-            0,
-            HWND::default(),
+            WINDOW_STYLE::default(),
+            0, 0,
+            0, 0,
+            HWND_MESSAGE,
             HMENU::default(),
             hinstance,
             None
@@ -63,8 +99,17 @@ fn main() -> Result<()> {
 
     unsafe { Shell_NotifyIconW(NIM_ADD, &notify_icon_data).ok()? };
 
+    let data = TrayData {
+        menu: Menu::new()?,
+    };
+
     unsafe {
-        SetWindowSubclass(hwnd, Some(tray_subclass_proc), TRAY_SUBCLASS_ID, 0).ok()?;
+        SetWindowSubclass(
+            hwnd,
+            Some(tray_subclass_proc),
+            TRAY_SUBCLASS_ID,
+            Box::into_raw(Box::new(data)) as _)
+            .ok()?;
     }
 
     unsafe {
@@ -111,19 +156,46 @@ impl ClickType {
 }
 
 unsafe extern "system" fn tray_subclass_proc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM, _id: usize, subclass_input_ptr: usize) -> LRESULT {
-
+    let subclass_input_ptr = subclass_input_ptr as *mut TrayData;
+    //println!("msg: {}", msg);
     match msg {
-        WM_DESTROY => println!("Destroyed"),
+        WM_DESTROY => {
+            drop(Box::from_raw(subclass_input_ptr));
+            println!("Destroyed");
+        },
         _ if msg == *S_U_TASKBAR_RESTART => println!("Taskbar restarted"),
         WM_USER_TRAY_ICON => if let Some(click) = ClickType::from_lparam(lparam) {
             println!("click: {:?}", click);
-            if click == ClickType::Double {
-                unsafe { PostMessageW(HWND::default(), WM_QUIT, WPARAM::default(), LPARAM::default()).unwrap() };
+            if click == ClickType::Right {
+                let mut cursor = POINT::default();
+                GetCursorPos(&mut cursor).unwrap();
+                SetForegroundWindow(hwnd).unwrap();
+                let hmenu = (*subclass_input_ptr).menu.hmenu;
+                TrackPopupMenu(
+                    hmenu,
+                    TPM_BOTTOMALIGN | TPM_LEFTALIGN,
+                    cursor.x, cursor.y,
+                    0,
+                    hwnd,
+                    None
+                ).unwrap();
+            }
+        }
+        WM_COMMAND => {
+            let id = LOWORD(wparam.0 as _);
+            println!("Command: {}", id);
+            if id == 42 {
+                PostMessageW(HWND::default(), WM_QUIT, WPARAM::default(), LPARAM::default()).unwrap();
             }
         }
         _ => {}
     }
     DefSubclassProc(hwnd, msg, wparam, lparam)
+}
+
+#[allow(non_snake_case)]
+pub fn LOWORD(dword: u32) -> u16 {
+    (dword & 0xFFFF) as u16
 }
 
 static S_U_TASKBAR_RESTART: Lazy<u32> = Lazy::new(|| unsafe { RegisterWindowMessageA(s!("TaskbarCreated")) });
