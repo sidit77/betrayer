f393c3fa305eefc61c9c5cc4c01487e5109cb9ee
sidit77 <sidit77@gmail.com>
2024-10-23 17:12:50 +0200

    updated dependencies

--- examples/winit.rs
+++ examples/winit.rs
@@ -1,10 +1,12 @@
 use anyhow::Result;
 use betrayer::winit::WinitTrayIconBuilderExt;
-use betrayer::{Icon, Menu, MenuItem, TrayEvent, TrayIconBuilder};
+use betrayer::{Icon, Menu, MenuItem, TrayEvent, TrayIcon, TrayIconBuilder};
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
-use winit::event::Event;
-use winit::event_loop::{ControlFlow, EventLoopBuilder};
+use winit::application::ApplicationHandler;
+use winit::event::{WindowEvent};
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::WindowId;
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 enum Signal {
@@ -19,10 +21,9 @@ fn main() -> Result<()> {
         .with_level(LevelFilter::Debug)
         .init()?;
 
-    let event_loop = EventLoopBuilder::with_user_event().build()?;
+    let event_loop = EventLoop::with_user_event().build()?;
 
-    let mut selected = 0;
-
+    let selected = 0;
     let tray = TrayIconBuilder::new()
         .with_icon(Icon::from_rgba(vec![255u8; 32 * 32 * 4], 32, 32)?)
         .with_tooltip("Demo System Tray")
@@ -36,26 +37,34 @@ fn main() -> Result<()> {
     //})?;
 
     event_loop.set_control_flow(ControlFlow::Wait);
-    event_loop.run(|event, evtl| match event {
-        Event::UserEvent(event) => {
-            log::info!("tray event: {:?}", event);
-            if let TrayEvent::Menu(signal) = event {
-                match signal {
-                    Signal::Profile(i) => {
-                        if selected != i {
-                            selected = i;
-                            tray.set_tooltip(format!("Active Profile: {selected}"));
-                            tray.set_menu(build_menu(selected));
-                        }
+    event_loop.run_app(&mut App { tray, selected })?;
+    Ok(())
+}
+
+struct App {
+    tray: TrayIcon<Signal>,
+    selected: u32
+}
+
+impl ApplicationHandler<TrayEvent<Signal>> for App {
+    fn resumed(&mut self, _event_loop: &ActiveEventLoop) { }
+    fn user_event(&mut self, event_loop: &ActiveEventLoop, event: TrayEvent<Signal>) {
+        log::info!("tray event: {:?}", event);
+        if let TrayEvent::Menu(signal) = event {
+            match signal {
+                Signal::Profile(i) => {
+                    if self.selected != i {
+                        self.selected = i;
+                        self.tray.set_tooltip(format!("Active Profile: {}", self.selected));
+                        self.tray.set_menu(build_menu(self.selected));
                     }
-                    Signal::Open => {}
-                    Signal::Quit => evtl.exit()
                 }
+                Signal::Open => {}
+                Signal::Quit => event_loop.exit()
             }
         }
-        _ => {}
-    })?;
-    Ok(())
+    }
+    fn window_event(&mut self, _event_loop: &ActiveEventLoop, _window_id: WindowId, _event: WindowEvent) { }
 }
 
 fn build_menu(selected: u32) -> Menu<Signal> {
